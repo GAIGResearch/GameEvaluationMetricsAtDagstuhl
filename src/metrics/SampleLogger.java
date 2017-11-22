37122b0a0d4c746a9e91ef5da0141bd62cec6833
diegopliebana <email@example.com>
2017-11-22 20:59:25 +0100

    events and objects now logged

--- src/metrics/SampleLogger.java
+++ src/metrics/SampleLogger.java
@@ -7,6 +7,7 @@ package metrics;
 
 import java.util.*;
 
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry;
 import metrics.plot.MetricVisualiser;
 
 /**
@@ -27,6 +28,11 @@ public class SampleLogger implements GameLogger {
     String outcomeScField = "OutcomeUncertaintyScore";
     String outcomeStField = "OutcomeUncertaintyState";
 
+    int gameTick = 0;
+    ArrayList<String> eventsThisGame;
+    ArrayList<String> objectsThisGame;
+
+
     DataWriter dataWriter = new DataWriter();
 
 
@@ -43,23 +49,102 @@ public class SampleLogger implements GameLogger {
         measures.get(outcomeScField).add(state.getOutcomeUncertaintyScore());
 
         // CODE IN PROGRESS:
-//        for(GameEvent ge : state.getGameEvents())
-//        {
-//            if(!measures.containsKey(ge.name))
-//                measures.put(ge.name, new ArrayList<>());
-//            measures.get(ge.name).add(ge.time);
-//        }
+
+        // Manage events
+        manageEvents(state);
+
+        // Manage game objects:
+        manageObjects(state);
 
         if (state != null){
             visualiser.update(state);
         }
+        gameTick++;
 
         return this;
     }
 
+    private void manageEvents(LoggableGameState state)
+    {
+        ArrayList<String> eventsThisTick = new ArrayList<>();
+        if(state.getGameEvents() != null) for(GameEvent ge : state.getGameEvents())
+        {
+            //Add 0.0 up to this point if it's new, and 0.0 for this tick
+            if(!measures.containsKey(ge.name))
+            {
+                measures.put(ge.name, new ArrayList<>());
+                for(int i = 0; i <= gameTick; ++i)
+                    measures.get(ge.name).add(0.0);
+            }else{
+                measures.get(ge.name).add(0.0);
+            }
+
+            //Keep a log of all events in the game.
+            if(!eventsThisGame.contains(ge.name))
+            {
+                eventsThisGame.add(ge.name);
+            }
+
+            //Update the events occurrences:
+            double howManyThisTick = measures.get(ge.name).get(gameTick);
+            measures.get(ge.name).set(gameTick, howManyThisTick + 1.0);
+
+            //Track of the events of this tick
+            eventsThisTick.add(ge.name);
+        }
+
+        //Need to update those known events that didn't happen this tick.
+        for(String name : eventsThisGame)
+        {
+            if(!eventsThisTick.contains(name))
+                measures.get(name).add(0.0);
+        }
+    }
+
+
+    private void manageObjects(LoggableGameState state)
+    {
+        ArrayList<String> objectsThisTick = new ArrayList<>();
+
+        HashMap<String, Integer> gameObjects = state.getGameObjects();
+
+        Iterator<Map.Entry<String, Integer>> itObjects = gameObjects.entrySet().iterator();
+
+        while(itObjects.hasNext())
+        {
+            Map.Entry<String, Integer> objectOcc = itObjects.next();
+
+            if(!measures.containsKey(objectOcc.getKey()))
+            {
+                measures.put(objectOcc.getKey(), new ArrayList<>());
+                for(int i = 0; i <= gameTick; ++i)
+                    measures.get(objectOcc.getKey()).add(0.0);
+            }
+
+            //Log of all objects in this game
+            if(!objectsThisGame.contains(objectOcc.getKey()))
+            {
+                objectsThisGame.add(objectOcc.getKey());
+            }
+
+            measures.get(objectOcc.getKey()).add((double) objectOcc.getValue());
+
+            objectsThisTick.add(objectOcc.getKey());
+        }
+
+        //Need to update those known objects that didn't appear on this tick.
+        for(String name : objectsThisGame)
+        {
+            if(!objectsThisTick.contains(name))
+                measures.get(name).add(0.0);
+        }
+    }
+
+
     @Override
     public GameLogger startGame() {
 
+        gameTick = 0;
         measures = new HashMap<>();
         measures.put(scoreField, new ArrayList<>());
         measures.put(actionField, new ArrayList<>());
@@ -68,6 +153,8 @@ public class SampleLogger implements GameLogger {
         measures.put(outcomeScField, new ArrayList<>());
         measures.put(outcomeStField, new ArrayList<>());
 
+        eventsThisGame = new ArrayList<>();
+        objectsThisGame = new ArrayList<>();
         visualiser = new MetricVisualiser();
 
         return this;
