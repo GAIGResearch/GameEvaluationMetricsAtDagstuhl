a7b43a457bcbea9ef9c3f0bc0753756b88146b50
diegopliebana <email@example.com>
2017-11-22 11:50:47 +0100

    metrics to measures

--- src/metrics/SampleLogger.java
+++ src/metrics/SampleLogger.java
@@ -15,7 +15,7 @@ import metrics.plot.MetricVisualiser;
  */
 public class SampleLogger implements GameLogger {
 
-    ArrayList<Integer> actionList;
+    //ArrayList<Integer> actionList;
 //    HashMap<Integer, Integer> actionMap;
 
     ArrayList<GameEvent[]> gameEvents;
@@ -37,6 +37,8 @@ public class SampleLogger implements GameLogger {
 
     String scoreField = "Score";
 
+    String actionField = "Action";
+
 
     public SampleLogger() {
         // need to build the things that we wish to log
@@ -48,11 +50,22 @@ public class SampleLogger implements GameLogger {
     @Override
     public GameLogger logState(LoggableGameState state) {
 
-        actionList.add(state.allActions()[0]);
+        //actionList.add(state.allActions()[0]);
         gameEvents.add(state.getGameEvents());
         gameObjects.add(state.getGameObjects());
 
         measures.get(scoreField).add(state.getScore());
+        measures.get(actionField).add((double) state.allActions()[0]);
+
+//        for(GameEvent ge : state.getGameEvents())
+//        {
+//            if(!measures.containsKey(ge.name))
+//                measures.put(ge.name, new ArrayList<>());
+//            measures.get(ge.name).add(ge.time);
+//        }
+
+
+        //for()
 
         if (state != null){
             visualiser.update(state);
@@ -66,9 +79,10 @@ public class SampleLogger implements GameLogger {
 
         measures = new HashMap<>();
         measures.put(scoreField, new ArrayList<>());
+        measures.put(actionField, new ArrayList<>());
 
 
-        actionList = new ArrayList<>();
+        //actionList = new ArrayList<>();
         gameEvents = new ArrayList<>();
         gameObjects = new ArrayList<>();
         visualiser = new MetricVisualiser();
@@ -94,74 +108,73 @@ public class SampleLogger implements GameLogger {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void debug()
-    {
-        /// PRINTING ENTROPY
-        double normalisedEntropy = metrics.Utils.normalisedEntropy(actionList);
-        System.out.println("[LOGGER] Entropy of actions: " + normalisedEntropy);
-//        MetricVisualiser visualiser = Met
-
-        Utils.printLogMsg("Entropy of actions: " + normalisedEntropy);
-
-        double[] scoreDiff = metrics.Utils.differentialArray(scoreHistory);
-        Utils.printLogMsgWithTag("Score changes per game tick: ", scoreDiff);
-        
-        System.out.print("Decisiveness: ");
-        for(double dec: decisivenessHistory){
-            System.out.print(dec + ", ");
-        }
-        System.out.print("\n");
-        
-        System.out.print("Convergence: ");
-        for(double dec: convergenceHistory){
-            System.out.print(dec + ", ");
-        }
-        System.out.print("\n");
-
-
-        /// PRINTING GAME OBJECTS
-        //ArrayList<Map<String, Integer>> gameObjects;
-        int timeSteps = 0;
-        for(Map<String, Integer> gObjs : gameObjects)
-        {
-            if(gObjs != null)
-            {
-                System.out.print("[LOGGER] " + timeSteps + " ");
-                Iterator<Map.Entry<String, Integer>> objsIt = gObjs.entrySet().iterator();
-                while(objsIt.hasNext())
-                {
-                    Map.Entry<String, Integer> entry = objsIt.next();
-                    System.out.print(entry.getKey() + ": " + entry.getValue() + "; ");
-                }
-                System.out.println();
-
-            }
-            timeSteps++;
-        }
-
-        /// PRINTING GAME EVENTS
-        timeSteps = 0;
-        for(GameEvent[] ges : gameEvents)
-        {
-            if(ges != null)
-            {
-                System.out.print("[LOGGER] " + timeSteps + " ");
-                for(GameEvent ge : ges) System.out.print(ge.name + " at position " + ge.avatarPosition + "; ");
-                System.out.println();
-            }
-            timeSteps++;
-        }
-
-        System.out.println();
-        System.out.println("Score frequency distribution");
-        System.out.println(new FrequencyMap().add(scoreDiff).getMap());
-
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    private void debug()
+//    {
+//        /// PRINTING ENTROPY
+//        double normalisedEntropy = metrics.Utils.normalisedEntropy(actionList);
+//        System.out.println("[LOGGER] Entropy of actions: " + normalisedEntropy);
+////        MetricVisualiser visualiser = Met
+//
+//        Utils.printLogMsg("Entropy of actions: " + normalisedEntropy);
+//
+//        double[] scoreDiff = metrics.Utils.differentialArray(scoreHistory);
+//        Utils.printLogMsgWithTag("Score changes per game tick: ", scoreDiff);
+//
+//        System.out.print("Decisiveness: ");
+//        for(double dec: decisivenessHistory){
+//            System.out.print(dec + ", ");
+//        }
+//        System.out.print("\n");
+//
+//        System.out.print("Convergence: ");
+//        for(double dec: convergenceHistory){
+//            System.out.print(dec + ", ");
+//        }
+//        System.out.print("\n");
+//
+//
+//        /// PRINTING GAME OBJECTS
+//        //ArrayList<Map<String, Integer>> gameObjects;
+//        int timeSteps = 0;
+//        for(Map<String, Integer> gObjs : gameObjects)
+//        {
+//            if(gObjs != null)
+//            {
+//                System.out.print("[LOGGER] " + timeSteps + " ");
+//                Iterator<Map.Entry<String, Integer>> objsIt = gObjs.entrySet().iterator();
+//                while(objsIt.hasNext())
+//                {
+//                    Map.Entry<String, Integer> entry = objsIt.next();
+//                    System.out.print(entry.getKey() + ": " + entry.getValue() + "; ");
+//                }
+//                System.out.println();
+//
+//            }
+//            timeSteps++;
+//        }
+//
+//        /// PRINTING GAME EVENTS
+//        timeSteps = 0;
+//        for(GameEvent[] ges : gameEvents)
+//        {
+//            if(ges != null)
+//            {
+//                System.out.print("[LOGGER] " + timeSteps + " ");
+//                for(GameEvent ge : ges) System.out.print(ge.name + " at position " + ge.avatarPosition + "; ");
+//                System.out.println();
+//            }
+//            timeSteps++;
+//        }
+//
 //        System.out.println();
-    }
+//        System.out.println("Score frequency distribution");
+//        System.out.println(new FrequencyMap().add(scoreDiff).getMap());
+//
+//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+////        System.out.println();
+//    }
 
     public void resetRecords() {
-        actionList = new ArrayList<>();
         scoreHistory = new ArrayList<>();
         gameEvents = new ArrayList<>();
         gameObjects = new ArrayList<>();
